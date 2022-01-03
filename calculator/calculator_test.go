a5da2d9dbe58bd006a3760ee3f660b22a5984c79
Ward Fenton <ward.fenton@gmail.com>
2022-01-03 18:48:08 -0500

    table driven testing in go

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,31 +6,28 @@ import (
 )
 
 type TestCase struct {
+	name     string
 	value    int
 	expected bool
 	actual   bool
 }
 
 func TestCalculateIsArmstrong(t *testing.T) {
-	t.Run("should return true for 371", func(t *testing.T) {
-		testCase := TestCase{
-			value:    371,
-			expected: true,
+	t.Run("test for all 3 digit armstrong numbers", func(t *testing.T) {
+		tests := []TestCase{
+			TestCase{name: "Testing value for: 153", value: 153, expected: true},
+			TestCase{name: "Testing value for: 370", value: 370, expected: true},
+			TestCase{name: "Testing value for: 371", value: 371, expected: true},
+			TestCase{name: "Testing value for: 407", value: 407, expected: true},
 		}
-		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-		if testCase.actual != testCase.expected {
-			t.Fail()
-		}
-	})
 
-	t.Run("should return true for 370", func(t *testing.T) {
-		testCase := TestCase{
-			value:    370,
-			expected: true,
-		}
-		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-		if testCase.actual != testCase.expected {
-			t.Fail()
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				actual := calculator.CalculateIsArmstrong(test.value)
+				if test.expected != actual {
+					t.Fail()
+				}
+			})
 		}
 	})
 }
