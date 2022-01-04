77ca38ef38402aa5561514c93c6ddb075e0b98d6
Ward Fenton <ward.fenton@gmail.com>
2022-01-03 20:17:35 -0500

    getting started with TestMain

--- yamltohtml/yamltohtml_test.go
+++ yamltohtml/yamltohtml_test.go
@@ -1,6 +1,8 @@
 package yamltohtml_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/wfen/go-testing-bible/yamltohtml"
@@ -10,6 +12,13 @@ type TestCase struct {
 	desc     string
 	path     string
 	expected string
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("Hello World")
+	ret := m.Run()
+	fmt.Println("Tests have executed")
+	os.Exit(ret)
 }
 
 func TestYamlToHTML(t *testing.T) {
